98c99228ccab3de07d55b2565c7c4d06c1241b5e
Vasilevykh-M <90954647+Vasilevykh-M@users.noreply.github.com>
2021-10-22 12:53:13 +0500

    Add files via upload

---
+++ laba_2_public_users.sql
@@ -0,0 +1,8 @@
+insert into public.users (id, login, password, time)
+values  (2, 'abc', '$2a$11$.Z12ctloFwudD                                        ', '2021-09-21 08:03:46.464472'),
+        (3, 'abcd', '$2a$11$w45BqniE2ODRw                                        ', '2021-09-21 13:04:46.170122'),
+        (4, 'abcde', '$2a$11$YRCFNq8NEVeZY                                        ', '2021-09-21 11:49:23.932055'),
+        (5, 'aa', '$2a$11$y2H9Gl7NQd/vX                                        ', '2021-09-21 15:20:43.004030'),
+        (6, 'Murad', '$2a$10$UgM5UyvN2GTLj                                        ', '2021-09-22 07:25:50.091925'),
+        (7, 'abcdefg', '$2a$11$AgySB6k0u5YX2                                        ', '2021-09-23 13:06:13.750476'),
+        (8, 'ФИО', '$2a$11$FFalmbowphtcw9yIzzX5r.nE6ElGODqFLyiOTVPZ6XIJIna4qaitu', '2021-10-08 09:38:01.782458');
